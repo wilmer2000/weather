9e0eecafeca76b09ee5c2d4d1f7561490620a429
Wilmer Blanco <wilmervzla2000@gmail.com>
2024-10-16 01:24:11 -0300

    Remove proxy configuration and update weather service
    Removed the local proxy configuration for weather and country APIs. Updated the weather service to use direct API URLs and include necessary headers for authentication. Modified environment variables to reflect these changes.

--- env/enviroment.ts
+++ env/enviroment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  weatherApi: '/weather/api/',
-  countryApi: '/country/api/',
+  weatherApi: 'https://open-weather13.p.rapidapi.com',
+  countryApi: 'https://country-state-city-search-rest-api.p.rapidapi.com',
 };
--- src/app/features/weather/services/weather.service.ts
+++ src/app/features/weather/services/weather.service.ts
@@ -1,10 +1,11 @@
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../../../env/enviroment';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { LocalStorageService } from '../../../core/services/local-storage.service';
-import { BehaviorSubject, catchError, Observable } from 'rxjs';
-import { WEATHER_KEY_LOCAL_STORAGE, WEATHER_MOCK_DATA } from '../constants/weather.constant';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { WEATHER_KEY_LOCAL_STORAGE } from '../constants/weather.constant';
 import { Weather } from '../classes/weather.class';
+import { API_KEYS } from '../../../../../api-keys';
 
 @Injectable({
   providedIn: 'root',
@@ -13,27 +14,27 @@ export class WeatherService {
   private readonly apiUrl = environment.weatherApi;
   private readonly http: HttpClient = inject(HttpClient);
   private readonly localStorage: LocalStorageService = inject(LocalStorageService);
+  private readonly headers: HttpHeaders = new HttpHeaders()
+    .set('x-rapidapi-host', 'open-weather13.p.rapidapi.com')
+    .set('x-rapidapi-key', API_KEYS.prod);
   private dataSubject: BehaviorSubject<Weather | null> = new BehaviorSubject<Weather | null>(null);
 
   data$: Observable<Weather | null> = this.dataSubject.asObservable();
 
   getWeatherByCity(city: string): void {
-    this.http.get(`${this.apiUrl}/${city}`)
-      .pipe(catchError((err: HttpErrorResponse) => {
-        throw err;
-      }))
+    this.http.get(`${this.apiUrl}/city/${city}/EN`, { headers: this.headers })
       .subscribe((data: any) => this.setWeatherToLocalStorage(data));
   }
 
   getCurrentWeather(): Observable<Weather | null> {
     if (!this.localStorage.hasItem(WEATHER_KEY_LOCAL_STORAGE)) {
-      this.getWeatherByCity('montevideo');
+      this.getWeatherByCity('landon');
     }
     return this.data$;
   }
 
   private setWeatherToLocalStorage(data: any): void {
-    const city = new Weather(WEATHER_MOCK_DATA);
+    const city = new Weather(data);
     this.localStorage.setItem(WEATHER_KEY_LOCAL_STORAGE, city);
     this.dataSubject.next(this.localStorage.getItem(WEATHER_KEY_LOCAL_STORAGE));
   }
